09dfa4ac57f2f6110e7d3196b42622f12b96d4e2
Renato-Alencar <renato.augustoalencar@gmail.com>
2021-03-05 18:26:01 -0300

    Ajusta a quantidade de cards e a dimensão das telas.

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -5,35 +5,17 @@ $('.carousel').slick ({
     slidesToShow: 6,
     slidesToScroll: 1,
     responsive: [
-      {
-        breakpoint: 1920,
-        settings: {
-          slidesToShow: 5,
-        }
-      },
-      {
-        breakpoint: 1366,
-        settings: {
-          slidesToShow: 4,
-        }
-      },
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-        }
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-        }
-      },
-      {
-        breakpoint: 540,
-        settings: {
-          slidesToShow: 2,
-        }
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
       }
-    ]
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+      }
+    }
+  ]
 });
