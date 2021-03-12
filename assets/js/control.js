c1638c4c20b1ed0b1f1c8dc0898619274968be38
Renato-Alencar <renato.augustoalencar@gmail.com>
2021-03-12 20:08:23 -0300

    Adiciona a lógica de acesso para a página de contato e para homepage.

--- assets/js/control.js
+++ assets/js/control.js
@@ -1,18 +1,36 @@
 const theme = document.querySelector('.theme')
+const contact = document.querySelector('#contact')
+const logo = document.querySelector('.logo')
+const homepage = document.querySelector('main')
+const form = document.querySelector('form')
+const body = document.querySelector('body')
+const color = document.querySelector('#mode')
+const text = document.querySelector('p')
+const submit = document.querySelector('#submit')
+
+submit.addEventListener('click', () => alert('Mensagem enviada com sucesso!'))
 
 theme.addEventListener('click', () => {
-   let body = document.querySelector('body').classList
-   let color = document.querySelector('#mode')
-   let text = document.querySelector('p')
-   
-   if(body == 'light-theme') {
-      text.innerHTML = 'DARK' 
+
+   if(body.classList == 'light-theme') {
+      text.innerHTML = 'DARK'
       color.innerHTML = 'dark_mode' 
-      body.add('dark-theme')
+      body.classList.add('dark-theme')
    }
    else {
-      text.innerHTML = 'LIGHT' 
+      text.innerHTML = 'LIGHT'
       color.innerHTML = 'light_mode'
-      body.remove('dark-theme') 
+      body.classList.remove('dark-theme') 
    }  
 })
+
+function toPressButton(hiddenPage, showPage) {
+   
+   if(hiddenPage.style.display != 'none') {
+      hiddenPage.style.display = 'none'
+      showPage.style.display = 'block'
+   }
+}
+
+contact.addEventListener('click', () => {toPressButton(homepage, form)})
+logo.addEventListener('click', () => {toPressButton(form, homepage)})
